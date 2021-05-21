0faea6b5327a5b402fd9b48a16c9b798362f2949
Akash52 <ac8572611@gmail.com>
2021-05-21 10:26:08 +0530

    Navbar & React Router setup

--- client/src/components/Layout/Navabr.js
+++ client/src/components/Layout/Navabr.js
@@ -6,7 +6,7 @@ const Navbar = () => {
 
   return (
     <>
-      <nav className="bg-blue-800 relative flex flex-wrap items-center justify-between px-2 py-3 bg-blueGray-500 mb-3">
+      <nav className="bg-black relative flex flex-wrap items-center justify-between px-2 py-3 bg-blueGray-500 mb-3">
         <div className="container px-4 mx-auto flex flex-wrap items-center justify-between">
           <div className="w-full relative flex justify-between lg:w-auto lg:static lg:block lg:justify-start">
             <Link
@@ -31,11 +31,11 @@ const Navbar = () => {
           <div
             className={
               'lg:flex flex-grow items-center' +
-              (navbarOpen ? ' flex' : ' hidden')
+              (navbarOpen ? ' flex ' : ' hidden')
             }
             id="example-navbar-danger"
           >
-            <ul className="flex flex-col lg:flex-row list-none lg:ml-auto">
+            <ul className="flex  flex-col lg:flex-row list-none lg:ml-auto">
               <li className="nav-item">
                 <Link
                   className="px-3 py-2 flex items-center text-xs uppercase font-bold leading-snug text-white hover:opacity-75"
