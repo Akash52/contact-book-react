27aaa5dd21be161089c58340968634d722832e07
Akash52 <ac8572611@gmail.com>
2021-05-21 09:18:49 +0530

    Add Contact Route

--- routes/contacts.js
+++ routes/contacts.js
@@ -27,9 +27,32 @@ router.get('/', auth, async (req, res) => {
 //@desc Add new Contact
 //@access Private
 
-router.post('/', (req, res) => {
-  res.send('Add contact')
-})
+router.post(
+  '/',
+  [auth, [check('name', 'Name is required').not().isEmpty()]],
+  async (req, res) => {
+    const errors = validationResult(req)
+
+    if (!errors.isEmpty()) {
+      return res.status(400).json({ errors: errors.array() })
+    }
+    const { name, email, phone, type } = req.body
+    try {
+      const newContact = new Contact({
+        name,
+        email,
+        phone,
+        type,
+        user: req.user.id,
+      })
+      const contact = await newContact.save()
+      res.json(contact)
+    } catch (error) {
+      console.error(error.message)
+      res.status(500).send('server Error')
+    }
+  }
+)
 
 //@route PUT api/contacts/:id
 //@desc Update contact
