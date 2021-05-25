20626be2af1c4a478106bccc11f23f3d480d10e3
Akash52 <ac8572611@gmail.com>
2021-05-25 09:15:46 +0530

    client

--- client/README.md
+++ client/README.md
@@ -1,18 +1,1 @@
-# Task Tracker App using React & TailwindCSS
 
-Simple task tracker app track your daily task
-
-[Live Demo](https://quirky-brown-1226cd.netlify.app/)
-
-
-
-```
-# Install dependencies
-npm install
-
-# Watch CSS & run server on :3000
-npm start
-
-# Build CSS and assets
-npm run build
-```
--- client/src/App.js
+++ client/src/App.js
@@ -1,14 +1,11 @@
-import { BrowserRouter as Router } from 'react-router-dom'
 import React from 'react'
-import Navbar from './components/Layout/Navabr'
+import Card from './components/Layout/Card/Card'
 
 const App = () => {
   return (
-    <>
-      <Router>
-        <Navbar />
-      </Router>
-    </>
+    <div>
+      <Card />
+    </div>
   )
 }
 
